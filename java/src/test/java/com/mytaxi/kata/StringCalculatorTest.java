10ed3627fc64326a45d7b7b6aa9b60260fe155d4
Ricardo Iglesias <r.iglesias@mytaxi.com>
2018-02-22 12:46:55 +0100

    adding empty test

--- java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
+++ java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
@@ -1,6 +1,19 @@
 package com.mytaxi.kata;
 
+import org.junit.Test;
+
 public class StringCalculatorTest
 {
+
+    @Test
+    public void shouldAddTwoNumbers()
+    {
+
+        // given
+
+        //when
+
+        //then
+    }
 
 }
