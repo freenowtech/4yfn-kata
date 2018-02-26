028c658f3915e1fe073a274ffc411b177f8d100f
MyTaxi <mytaxiguestbcn@mytaxi.com>
2018-02-26 12:58:20 +0100

    Step 1

--- java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
+++ java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.mytaxi.kata;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class StringCalculatorTest
 {
@@ -8,12 +9,23 @@ public class StringCalculatorTest
     @Test
     public void shouldAddTwoNumbers()
     {
-
+        StringCalculator stringCalculator = new StringCalculator();
         // given
 
+        String voidString = "";
+        String a = "1";
+        String b = "1,2";
         //when
 
+        String resultVoid = "0";
+        String resultA = "1";
+        String resultB = "3";
+
         //then
+
+        assertEquals(stringCalculator.add(voidString, resultVoid));
+        assertEquals(stringCalculator.add(a, resultA));
+        assertEquals(stringCalculator.add(b, resultB));
     }
 
 }
