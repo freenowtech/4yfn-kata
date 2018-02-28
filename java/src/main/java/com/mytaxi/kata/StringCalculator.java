4454e433170e8a7073eec4132921fea030f95ded
MyTaxi <mytaxiguestbcn@mytaxi.com>
2018-02-28 16:41:09 +0100

    work by jesus ligero

--- java/src/main/java/com/mytaxi/kata/StringCalculator.java
+++ java/src/main/java/com/mytaxi/kata/StringCalculator.java
@@ -5,6 +5,27 @@ public class StringCalculator
 
     public int calculate(String valueAsString) {
 
-        return 0;
+        if (valueAsString.isEmpty())
+                return 0;
+
+
+
+        if (valueAsString.endsWith("\n"))
+            return -1;
+
+        String[] numeros =valueAsString.split(",");
+        int len= numeros.length;
+        int i = 0;
+        int res =0;
+        while (i<len)
+        {
+
+            res = res + Integer.parseInt(numeros[i]);
+            i++;
+        }
+
+        return res;
+
+
     }
 }
--- java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
+++ java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
@@ -1,14 +1,71 @@
 package com.mytaxi.kata;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class StringCalculatorTest
 {
 
+    private StringCalculator stringCalculator;
+
+
+    @Test
+    public void shouldADD0numb()
+    {
+        stringCalculator = new StringCalculator();
+
+        Assert.assertEquals(0,stringCalculator.calculate(""));
+        // given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void shouldAdd1Numbers()
+    {
+        stringCalculator = new StringCalculator();
+
+        Assert.assertEquals(12,stringCalculator.calculate("12"));
+        // given
+
+        //when
+
+        //then
+    }
+
     @Test
     public void shouldAddTwoNumbers()
     {
+        stringCalculator = new StringCalculator();
 
+        Assert.assertEquals(3,stringCalculator.calculate("1,2"));
+        // given
+
+        //when
+
+        //then
+    }
+    @Test
+    public void shouldAddNnumbers()
+    {
+        stringCalculator = new StringCalculator();
+
+        Assert.assertEquals(55,stringCalculator.calculate("1,2,3,4,5,6,7,8,9,10"));
+        // given
+
+        //when
+
+        //then
+    }
+    @Test
+    public void shouldAddNqwithjumpanumbers()
+    {
+        stringCalculator = new StringCalculator();
+
+        Assert.assertEquals(11,stringCalculator.calculate("1\n"));
         // given
 
         //when
