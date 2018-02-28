32fb2bd93e62d9915115f109368aa94b7d26b4bb
MyTaxi <mytaxiguestbcn@mytaxi.com>
2018-02-28 18:06:52 +0100

    Step 2

--- java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
+++ java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class StringCalculatorTest
 {
 
@@ -22,4 +24,25 @@ public class StringCalculatorTest
         Assert.assertEquals(3, stringCalculator.calculate("1,2"));
     }
 
+    @Test
+    public void shouldAddAnyNumbers()
+    {
+        StringBuilder builder = new StringBuilder("");
+        int accum = 0;
+        Random rNumberElements = new Random();
+        Random rNumber = new Random();
+
+        int numberElements = rNumberElements.nextInt(200);
+        int number;
+
+        for (int i = 0 ; i < numberElements ; i++) {
+            number = rNumber.nextInt();
+            builder.append(number);
+            builder.append(',');
+            accum += number;
+        }
+        builder.deleteCharAt(builder.length()-1);
+
+        Assert.assertEquals(accum, stringCalculator.calculate(builder.toString()));
+    }
 }
