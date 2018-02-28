dc5a8811ad7e8f851358f77c85b61de03e9a0871
MyTaxi <mytaxiguestbcn@mytaxi.com>
2018-02-28 17:58:51 +0100

    <message regarding the task you are solving>

--- java/src/main/java/com/mytaxi/kata/StringCalculator.java
+++ java/src/main/java/com/mytaxi/kata/StringCalculator.java
@@ -4,7 +4,13 @@ public class StringCalculator
 {
 
     public int calculate(String valueAsString) {
-
-        return 0;
+        int res = 0;
+        if (valueAsString.length() > 0) {
+            String[] numbers = valueAsString.split(",");
+            for (int i = 0; i < numbers.length; i++) {
+                res += Integer.valueOf(numbers[i]);
+            }
+        }
+        return res;
     }
 }
--- java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
+++ java/src/test/java/com/mytaxi/kata/StringCalculatorTest.java
@@ -1,19 +1,25 @@
 package com.mytaxi.kata;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest
 {
 
+    public StringCalculator stringCalculator;
+
+    @Before
+    public void init() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void shouldAddTwoNumbers()
     {
-
-        // given
-
-        //when
-
-        //then
+        Assert.assertEquals(0, stringCalculator.calculate(""));
+        Assert.assertEquals(1, stringCalculator.calculate("1"));
+        Assert.assertEquals(3, stringCalculator.calculate("1,2"));
     }
 
 }
